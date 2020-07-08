ebdc299efc71472d07efc0d956c007abf04db5af
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-08 05:17:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (phrase)
   if phrase.upcase != phrase
     "HUH?! SPEAK UP, SONNY!"
-elsif phrase.downcase != phrase
+elsif phrase.downcase == phrase
     "NO, NOT SINCE 1938!"
 else phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
