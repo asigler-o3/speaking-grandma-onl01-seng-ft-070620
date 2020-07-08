d085c2a57ef6591658759c914b786e9311b9e00e
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-08 05:04:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (phrase)
   if phrase.upcase == phrase
-
+elsif phrase.downcase == phrase
 end
 
 # Whatever you say to grandma, she should respond with
